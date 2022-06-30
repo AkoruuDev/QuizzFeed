b3a91ae083f453f9be7775eb91c21bcdb6cc5358
joaofp <jvfpdaemoon1@gmail.com>
2022-06-29 21:04:36 -0300

    CHORE [getAllQuizzesAPI] (joaofpdmn): função para pegar todos os quizzes

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,16 @@
+let allQuizzes = [];
+
 const APIprefix = 'https://mock-api.driven.com.br/api/v4/buzzquizz/' 
+
+let allQuizzesAPI = `${APIprefix}quizzes`
+allQuizzes.then(getAllQuizzesAPI);
+allQuizzes.catch(connectionError);
+
+function getAllQuizzes(response){
+    allQuizzes = allQuizzes.data;
+    console.log(response);
+}
+
+function connectionError(response){
+    
+}
