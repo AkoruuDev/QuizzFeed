36678aa98c6365b1111f2660551b58d152d16f92
Akoruu.Dev <akoruu.dev@gmail.com>
2022-06-29 21:19:19 -0300

    BAS [Novo quizz] (AkoruuDev): Criado área no index para criação de novo Quizz

--- scripts/script.js
+++ scripts/script.js
@@ -2,9 +2,8 @@ const APIprefix = 'https://mock-api.driven.com.br/api/v4/buzzquizz/'
 
 
 function createQuizz() {
-    let quizzes = document.querySelector(".without-quizz"); // mudar class para .quizzes
-    let createNewQuizz = document.querySelector(".your-created-quizzes");   // mudar class para .createNewQuizz
-
+    let quizzes = document.querySelector(".quizzes");
+    let createNewQuizz = document.querySelector(".newQuizz");
     quizzes.classList.add("none");
     createNewQuizz.classList.remove("none");
 }
